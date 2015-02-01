1d2508d62e0a92bd980669e83643795786dcdb7c
stefano92 <stefano496@gmail.com>
2015-02-01 11:22:52 +0100

    Added subtraction

--- Calcolatrice Natascia/src/Calculator.java
+++ Calcolatrice Natascia/src/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
 	public float addition (int addend1, int addend2) {
 		return addend1 + addend2;
 	}
+	
+	public float subtraction (int addend1, int addend2) {
+		return addend1 - addend2;
+	}
 }
